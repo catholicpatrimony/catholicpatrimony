f6ec5b1a8f9cca5b5e795ac0684d6f34823234d4
Anderson, Ben <ben.anderson@neustar.biz>
2015-07-03 14:17:39 -0400

    integrated comments (not live yet)

--- web/web/cp.js
+++ web/web/cp.js
@@ -1,7 +1,8 @@
-angular.module('cpApp', ['ngRoute'] //, function($compileProvider) {
+//angular.module('cpApp', ['ngRoute', 'ngDisqus'] //, function($compileProvider) {
   //}
-  )
-  .config(function($routeProvider, $compileProvider) {
+angular.module('cpApp', ['ngRoute', 'angularUtils.directives.dirDisqus'])
+  .config(function($routeProvider, $compileProvider, $locationProvider ) {
+  //.config(function($routeProvider, $compileProvider, $locationProvider, $disqusProvider ) {
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|itms):/);
     $routeProvider
       .when('/', {
@@ -14,11 +15,24 @@ angular.module('cpApp', ['ngRoute'] //, function($compileProvider) {
         controller:'ClassController',
         templateUrl:'web/classContent.html'
       })
+      .when('/session', {
+        reloadOnSearch: true,
+        controller:'SessionController',
+        templateUrl:'web/session.html'
+      })
       .otherwise({
         redirectTo:'/'
       });
+    /*
+    $locationProvider.html5Mode({
+      enabled: true
+    });
+    */
+    //$locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+    //$disqusProvider.setShortname('catholicpatrimony');
   })
-  .controller('MenuController', function($scope, $location, $routeParams) {
+  .controller('MenuController', function($scope, $location, $routeParams, $log) {
     $scope.cp = cp;
     $scope.dropDownMenu = [];
     $scope.topLevelMenu = [];
@@ -31,42 +45,84 @@ angular.module('cpApp', ['ngRoute'] //, function($compileProvider) {
         $scope.dropDownMenu.push(cp[i]);
       }
       if (cp[i].seriesData.include_zips && cp[i].seriesData.include_zips == "FALSE") {
-        console.log('include_zips = false');
+        $log.debug('include_zips = false');
         cp[i].seriesData.includeZips = false;
       } else {
-        console.log('include_zips = true');
+        $log.debug('include_zips = true');
         cp[i].seriesData.includeZips = true;
       }
     }
     $scope.dropDownClicked = function(idx) {
       setClass($scope, "dropDown");
-      $location.path('/class').search({'id' : idx});
+      $location.path('/class').search({'classId' : idx});
     }
     $scope.topLevelClassClicked = function(idx) {
       setClass($scope, "topLevelClass");
-      $location.path('/class').search({'id' : idx});
+      $location.path('/class').search({'classId' : idx});
     }
     $scope.aboutClicked = function(idx) {
       setClass($scope, "about");
       //$location.path('');
     }
-    console.log(cp);
+    $log.debug(cp);
   })
-  .controller('MainController', function($scope, $location, $routeParams) {
+  .controller('MainController', function($scope, $location, $routeParams, $log) {
   })
-  .controller('ClassController', function($scope, $location, $routeParams) {
-    console.log('in ClassController');
+  .controller('ClassController', function($scope, $location, $routeParams, $log) {
+    $log.debug('in ClassController');
     $scope.cp = cp;
-    console.log($routeParams);
-    if ($routeParams['id'] != null) {
-      console.log('found idx');
-      selectClass($scope, $routeParams.id);
+    $log.debug($routeParams);
+    if ($routeParams['classId'] != null) {
+      $log.debug('found idx');
+      selectClass($scope, $routeParams.classId);
+    }
+    if ($routeParams['enableComments'] != null) {
+      $scope.enableComments = true;
+    } else {
+      $scope.enableComments = false;
+    }
+    $log.debug('enableComments: ' + $scope.enableComments);
+
+    $scope.sessionClicked = function(sessionId) {
+      $location.path('/session').search({'classId' : $scope.classId, 'sessionId': sessionId});
+    }
+  })
+  .controller('SessionController', function($scope, $location, $routeParams, $log) {
+    $scope.myDisqus_contentLoaded = false;
+    $log.debug('in SessionController');
+    $scope.cp = cp;
+    $log.debug($routeParams);
+    /*
+    if ($routeParams['classId'] != null) {
+      $scope.session = 
+    }
+    */
+    if ($routeParams['classId'] != null) {
+      $log.debug('found idx');
+      selectClass($scope, $routeParams.classId);
+      if ($routeParams['sessionId'] != null) {
+        $scope.c = $scope.classes[$routeParams['sessionId']];
+        $scope.myDisqus_identifier = 'cp.com.session_' + $routeParams['classId'] + '_' + $routeParams['sessionId'];
+        $scope.myDisqus_title = $scope.c.title;
+        $scope.myDisqus_url = $location.absUrl();
+        $log.debug($scope.myDisqus_identifier);
+        $log.debug('d1');
+        $log.debug($scope.myDisqus_title);
+        document.title = $scope.myDisqus_title
+        $log.debug('d2');
+        $log.debug($scope.myDisqus_url);
+        $log.debug('d3');
+        $scope.myDisqus_contentLoaded = true;
+      }
     }
   })
 
 var selectClass = function(scope, idx) {
-  console.log(idx);
-  console.log(scope.cp[idx]);
+  /*
+  $log.debug(idx);
+  $log.debug(scope.cp[idx]);
+  */
+  scope.classId = idx;
   scope.seriesSelected = cp[idx];
   scope.sd = cp[idx].seriesData;
   scope.classes = cp[idx].classes;
@@ -84,9 +140,9 @@ var setClass = function(scope, tlm) {
     scope.topLevelClass="active";
   }
 
-
-
-  console.log("scope.aboutClass: " + scope.aboutClass  );
-  console.log("scope.dropDownClass: "+   scope.dropDownClass  );
-  console.log("scope.topLevelClass: "+  scope.topLevelClass  );
+  /*
+  $log.debug("scope.aboutClass: " + scope.aboutClass  );
+  $log.debug("scope.dropDownClass: "+   scope.dropDownClass  );
+  $log.debug("scope.topLevelClass: "+  scope.topLevelClass  );
+  */
 }

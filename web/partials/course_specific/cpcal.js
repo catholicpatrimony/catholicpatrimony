8c0c92bc1178d08392be6429cedacff166f44d1f
Anderson, Ben <Ben.Anderson@team.neustar>
2017-10-25 10:37:21 -0400

    fixing a cbp issue

--- web/partials/course_specific/cpcal.js
+++ web/partials/course_specific/cpcal.js
@@ -361,7 +361,7 @@ cpApp.controller('DailyHomiliesController', function($scope, $location, $routePa
       $scope.searchRelated(d);
       $scope.modalInstance = $uibModal.open({
         animation: true,
-        templateUrl: 'partials/course_specific/daily_session.html?cbp=20171023i',
+        templateUrl: 'partials/course_specific/daily_session.html?cbp=20171024b',
         controller: ModalInstanceCtrl,
         size: 'lg',
         scope: $scope,
